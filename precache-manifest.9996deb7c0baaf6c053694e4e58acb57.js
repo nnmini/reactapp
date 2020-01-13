f04373d9b6f39d439b6662fbc2afd7f0c8d7ea57
nnmini <namini.niranjan@gmail.com>
2020-01-12 19:10:55 -0800

    Updates

---
+++ precache-manifest.9996deb7c0baaf6c053694e4e58acb57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9806041891d45d3244ccbf4b0fbdfd7b",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "defa7821723d1e03b0ea",
+    "url": "/reactapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1a8c66f7b8178b5f2508",
+    "url": "/reactapp/static/js/2.58e05b40.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactapp/static/js/2.58e05b40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "defa7821723d1e03b0ea",
+    "url": "/reactapp/static/js/main.02dd5c08.chunk.js"
+  },
+  {
+    "revision": "6834698eef5b01fb48c0",
+    "url": "/reactapp/static/js/runtime-main.f31e0828.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
